3ff64e7491fc946364e4dce49c5280fbb5f2b7dc
gizm00 <4462255+gizm00@users.noreply.github.com>
2021-04-26 08:49:00 -0700

    image and notebook updates, modifing merge code

--- notebooks/camping/util/distance.py
+++ notebooks/camping/util/distance.py
@@ -1,16 +1,19 @@
 import geopandas as gpd
 
-def distance_merge(to_buffer, left, max_distance, lsuffix, rsuffix):
+def distance_merge(nf_data, ridb_data, max_distance, lsuffix='ridb', rsuffix='nf'):
     # https://stackoverflow.com/questions/58848416/filter-and-merge-points-from-two-dataframe-within-a-specific-distance-in-geopand
-    buffer_df = gpd.GeoDataFrame(to_buffer, geometry=gpd.points_from_xy(to_buffer.FacilityLongitude, to_buffer.FacilityLatitude))
-    left_df = gpd.GeoDataFrame(left, geometry=(gpd.points_from_xy(left.FacilityLongitude, left.FacilityLatitude)))
-    buffer_df.crs = left_df.crs = {'init': 'epsg:4326'} 
+    ridb_data = ridb_data[['FacilityID', 'CampsiteID', 'FacilityLatitude', 'FacilityLongitude']]
+    buffer_df = gpd.GeoDataFrame(nf_data, geometry=gpd.points_from_xy(nf_data.FacilityLongitude, nf_data.FacilityLatitude))
+    ridb_data_df = gpd.GeoDataFrame(ridb_data, geometry=(gpd.points_from_xy(ridb_data.FacilityLongitude, ridb_data.FacilityLatitude)))
+    buffer_df.crs = ridb_data_df.crs = {'init': 'epsg:4326'} 
     buffer_df = buffer_df.to_crs({'init': 'epsg:3857'})
-    left_df = left_df.to_crs({'init': 'epsg:3857'})
+    ridb_data_df = ridb_data_df.to_crs({'init': 'epsg:3857'})
 
-    # Create a buffer around each point to perform intersects with point in left df
+    # Create a buffer around each point to perform intersects with point in ridb_data df
     buffer_df.geometry = buffer_df.geometry.buffer(max_distance)
 
     # Join the two Dataframes and convert back to original projection
-    merged = gpd.sjoin(left_df, buffer_df, how='left', op='intersects', lsuffix=lsuffix, rsuffix=rsuffix)
-    return merged.to_crs({'init': 'epsg:4326'}) # or whatever was used originally
+    merged = gpd.sjoin(ridb_data_df, buffer_df, how='left', op='intersects', lsuffix=lsuffix, rsuffix=rsuffix)
+    merged = merged.to_crs({'init': 'epsg:4326'})
+    return merged
+
